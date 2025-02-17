0fb3f4fd9f5c8b26b40148e8c7e68d40b9ac274d
Preeyanat-Krasaeging <Preeyanat.k@ku.th>
2025-02-17 14:04:49 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,54 +1,72 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
 const app = express();
 
 const port = 8000;
 app.use(bodyParser.json());
 
 let users = []
-let counter = 1
+let conn = null
+
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+        password: 'root',
+        database: 'webdb', 
+        port: 8830
+  })
+}
 
 /*
-GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
-POST /users สำหรับสร้าง users ใหม่บันทึกเข้าไป
-GET /users/:id สำหรับดึง users รายคนออกมา
-PUT /users/:id สำหรับแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
-DELETE /users/:id สำหรับลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
-*/
-
-
-
-//path = GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
-app.get('/users', (req, res) => {
-  res.json(users);
+app.get('/testdbnew',async (req, res) => {
+  try {
+      const results = await conn.query('SELECT * FROM users')
+      res.json(results[0])
+    } catch (error) {
+      console.log('error', error.message)
+      res.status(500).json({error: 'Error fetching users'})
+    }
 })
+ */ 
+//path = GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users')
+    res.json(results[0])
+})  
 
 // path = POST /users สำหรับสร้าง users ใหม่บันทึกเข้าไป
-app.post('/user', (req, res) => {
+app.post('/users', async (req, res) => {
     let user = req.body;
-    user.id = counter;
-    counter += 1
-    users.push(user);    
+    const results = await conn.query('INSERT INTO users SET ? ', user)  
+    console.log('results', results)
     res.json({
-        message: 'Create new user successfully',
-        user: user
-    });
+      message: 'Create user successfully',
+      data: results[0]
+    })
 })
 
-// path:PUT /user/:id ใช้สำหรับแก้ไขข้อมูล users โดยใช้ id เป็นตัวระบุ
-app.put('/user/:id', (req, res) => {
+//path = GET /u;sers/:id สำหรับ users รายคนออกมา
+app.get('/users/:id', (req, res) => {
   let id = req.params.id;
-  let updateUser = req.body;
-  // ค้นหา users ที่ต้องการแก้ไข
+  // ค้นหา users หรือ index ที่ต้องการดึงข้อมูล
   let selectedIndex = users.findIndex(user =>  user.id == id)
 
-  // แก้ไขข้อมูล user
-  if(updateUser.firstname) {
-    users[selectedIndex].firstname = updateUser.firstname
-  }
-  if(updateUser.lastname) {
-    users[selectedIndex].lastname = updateUser.lastname
-  }
+  res.json(users[selectedIndex])
+})
+
+
+// path:PUT /users/:id  สำหรับแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.put('/users/:id', (req, res) => {
+  let id = req.params.id;
+  let updateUser = req.body;
+  let selectedIndex = users.findIndex(user =>  user.id == id)
+
+    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
+    users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
+    users[selectedIndex].age = updateUser.age  || users[selectedIndex].age
+    users[selectedIndex].gender = updateUser.gender  || users[selectedIndex].gender 
   
   res.json({
       message: 'Update user successfully',
@@ -59,8 +77,8 @@ app.put('/user/:id', (req, res) => {
   })
 })
 
-//path:DELETE /user/:id ใช้สำหรับลบข้อมูล user โดยใช้ id เป็นตัวระบุ
-app.delete('/user/:id', (req, res) => {
+//path:DELETE /users/:id สำหรับลบข้อมูล users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.delete('/users/:id', (req, res) => {
   let id = req.params.id;
   // หา users ที่ต้องการลบ
   let selectedIndex = users.findIndex(user =>  user.id == id)
@@ -69,11 +87,12 @@ app.delete('/user/:id', (req, res) => {
   users.splice(selectedIndex, 1)
   res.json({
       message: 'Delete user successfully',      
-      indexDeleted: selectedIndex
-      
+      indexDeleted: selectedIndex     
   })
 })
 
-app.listen(port, (req, res) => { 
+app.listen(port, async (req, res) => { 
+    await initMySQL()
     console.log('Http Server is running on port ' + port)
 });
+
